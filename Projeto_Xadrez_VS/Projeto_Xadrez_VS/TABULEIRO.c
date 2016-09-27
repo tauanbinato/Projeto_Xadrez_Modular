b71e234102e1fd1ef61f58bf91f0e09556c29070
tauanbinato <tauanflores@gmail.com>
2016-09-26 22:07:04 -0300

    Comecei a adicionar os comentarios da TABULEIRO.C e .H

---
+++ Projeto_Xadrez_VS/Projeto_Xadrez_VS/TABULEIRO.c
@@ -0,0 +1,29 @@
+﻿/***************************************************************************
+*  $MCI Módulo de implementação: TAB Tabuleiro
+*
+*  Arquivo gerado:              TABULEIRO.c
+*  Letras identificadoras:      TAB
+*
+*  Nome da base de software:    Arcabouço para a automaçãoo de testes de programas redigidos em C
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\TABULEIRO.BSW
+*
+*  Projeto: INF 1301 / 1628 Automatiza��o dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: avs
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     
+*
+***************************************************************************/
+
+#include   <stdio.h>
+#include   <string.h>
+#include   <memory.h>
+#include   <malloc.h>
+#include   <assert.h>
+
+#define LISTA_OWN
+#include "TABULEIRO.h"
+#undef LISTA_OWN
+
--- Projeto_Xadrez_VS/Projeto_Xadrez_VS/TABULEIRO.h
+++ Projeto_Xadrez_VS/Projeto_Xadrez_VS/TABULEIRO.h
@@ -1,1 +1,35 @@
 #pragma once
+#if ! defined( TABULEIRO_ )
+#define TABULEIRO_
+/***************************************************************************
+*
+*  $MCD Módulo de definição: LIS  Lista duplamente encadeada
+*
+*  Arquivo gerado:              TABULEIRO.h
+*  Letras identificadoras:      TAB
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\TABULEIRO.BSW
+*
+*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: Tauan Binato , Lucas Palma , Julia Aleixo
+*
+*
+*  $ED Descrição do módulo
+*
+*
+***************************************************************************/
+
+#if defined( TABULEIRO_OWN )
+#define TABULEIRO_EXT
+#else
+#define TABULEIRO_EXT extern
+#endif
+
+#undef TABULEIRO_EXT
+
+/********** Fim do módulo de definição: TAB  Tabuleiro **********/
+
+#else
+#endif
