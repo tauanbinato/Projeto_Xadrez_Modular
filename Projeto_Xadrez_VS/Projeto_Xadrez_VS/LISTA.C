02a94a81bc4ae478f9dbf6ecd99d8e7d9788b600
Lucas Palma <lucaspalmamedeiros@hotmail.com>
2016-10-01 17:16:54 -0300

    back to reality

--- Projeto_Xadrez_VS/Projeto_Xadrez_VS/LISTA.C
+++ Projeto_Xadrez_VS/Projeto_Xadrez_VS/LISTA.C
@@ -84,7 +84,7 @@
    static void LiberarElemento( LIS_tppLista   pLista ,
                                 tpElemLista  * pElem   ) ;
 
-   static tpElemLista * CriarElemento( LIS_tppLista pLista ,
+   static tpElemLista * CriarElemento( LIS_tppLista* pLista ,
                                        void *       pValor  ) ;
 
    static void LimparCabeca( LIS_tppLista pLista ) ;
@@ -180,7 +180,7 @@
 *  Fun��o: LIS  &Inserir elemento ap�s
 *  ****/
 
-   LIS_tpCondRet LIS_InserirNo( LIS_tppLista pLista ,void * pValor        )
+   LIS_tpCondRet LIS_InserirNo( LIS_tppLista* pLista ,void * pValor        )
       
    {
       tpElemLista * pElem ;
@@ -189,10 +189,8 @@
          assert( pLista != NULL ) ;
       #endif
 
-		 printf("entrou para inserir no\n");
       /* Criar elemento a inserir ap�s */
          pElem = CriarElemento( pLista , pValor ) ;
-		 printf("criou o elemento a ser inserido");
 
          if ( pElem == NULL )
          {
@@ -201,29 +199,29 @@
 		
       /* Encadear o elemento ap�s o elemento */
 
-         if ( pLista->pElemCorr == NULL )
+         if ((*pLista)->pElemCorr == NULL )
          {
-            pLista->pOrigemLista = pElem ;
-            pLista->pFimLista = pElem ;
+			 (*pLista)->pOrigemLista = pElem ;
+			 (*pLista)->pFimLista = pElem ;
          } else
          {
-            if ( pLista->pElemCorr->pProx != NULL )
+            if ((*pLista)->pElemCorr->pProx != NULL )
             {
-               pElem->pProx  = pLista->pElemCorr->pProx ;
-               pLista->pElemCorr->pProx->pAnt = pElem ;
+               pElem->pProx  = (*pLista)->pElemCorr->pProx ;
+			   (*pLista)->pElemCorr->pProx->pAnt = pElem ;
             } else
             {
-               pLista->pFimLista = pElem ;
+				(*pLista)->pFimLista = pElem ;
             } /* if */
 
-            pElem->pAnt = pLista->pElemCorr ;
-            pLista->pElemCorr->pProx = pElem ;
+            pElem->pAnt = (*pLista)->pElemCorr ;
+			(*pLista)->pElemCorr->pProx = pElem ;
 
          } /* if */
 		
 		 pElem->pValor = pValor;
 
-		 pLista->pElemCorr = pElem ;
+		 (*pLista)->pElemCorr = pElem ;
 		 
          return LIS_CondRetOK ;
 
@@ -282,18 +280,18 @@
 *  Fun��o: LIS  &Obter refer�ncia para o valor contido no elemento
 *  ****/
 
-LIS_tpCondRet LIS_ObterNo( LIS_tppLista pLista, char* CharObtido ) {
+LIS_tpCondRet LIS_ObterNo( LIS_tppLista* pLista, char* CharObtido ) {
 #ifdef _DEBUG
 	   assert(pLista != NULL);
 #endif
 
-	   if (pLista->pElemCorr == NULL)
+	   if ((*pLista)->pElemCorr == NULL)
 	   {
 		   *CharObtido = NULL;
 		   return LIS_CondRetListaVazia;
 	   } /* if */
 
-	   *CharObtido = pLista->pElemCorr->pValor;
+	   *CharObtido = (*pLista)->pElemCorr->pValor;
 	  
 	   return LIS_CondRetOK;
 
@@ -389,7 +387,7 @@ LIS_tpCondRet LIS_AlterarElementoCorrente(LIS_tppLista pLista, char CharDado)
 	
 	/* Tratar se Lista Existe */
 	if (pLista == NULL) {
-		printf("\nNAO EXISTE");
+
 		return LIS_CondRetListaNaoExiste;
 	}
 	/* fim ativa: Tratar se Lista Existe */
@@ -398,11 +396,10 @@ LIS_tpCondRet LIS_AlterarElementoCorrente(LIS_tppLista pLista, char CharDado)
 
 	if (pLista->pElemCorr == NULL)
 	{
-		printf("\nVAZIAO DA MASSA");
 		return LIS_CondRetListaVazia;
 
 	} /* fim ativa: Tratar lista vazia */
-	printf("\nTA ROLANDO");
+
    
 	/* Tratar troca */
 	pLista->pElemCorr->pValor = CharDado;
@@ -440,7 +437,7 @@ LIS_tpCondRet LIS_AlterarElementoCorrente(LIS_tppLista pLista, char CharDado)
 *
 ***********************************************************************/
 
-   tpElemLista * CriarElemento( LIS_tppLista pLista ,
+   tpElemLista * CriarElemento( LIS_tppLista* pLista ,
                                 void *       pValor  )
    {
 
@@ -456,7 +453,7 @@ LIS_tpCondRet LIS_AlterarElementoCorrente(LIS_tppLista pLista, char CharDado)
       pElem->pValor = pValor ;
       pElem->pAnt   = NULL  ;
       pElem->pProx  = NULL  ;
-      pLista->numElem ++ ;
+	  (*pLista)->numElem ++ ;
 
       return pElem ;
 
