d5ab65086bade310bfb3f265259904f366e8c62e
tauanbinato <tauanflores@gmail.com>
2016-10-02 21:22:37 -0300

    Criando um modulo PECA

--- Projeto_Xadrez_VS/Projeto_Xadrez_VS/PECA.h
+++ Projeto_Xadrez_VS/Projeto_Xadrez_VS/PECA.h
@@ -1,1 +1,76 @@
+#pragma once
+#if ! defined( PECA_ )
+#define PECA_
+/***************************************************************************
+*
+*  $MCD Módulo de definição: PEC  Estrutura de uma Pca
+*
+*  Arquivo gerado:              PECA.h
+*  Letras identificadoras:      PECA
+*
+*  Nome da base de software:    Arcabouço para a automação de testes de programas redigidos em C
+*  Arquivo da base de software: D:\AUTOTEST\PROJETOS\PECA.BSW
+*
+*  Projeto: INF 1301 / 1628 Automatização dos testes de módulos C
+*  Gestor:  LES/DI/PUC-Rio
+*  Autores: Julia Aleixo , Lucas Palma , Tauan Binato
+*
+*  $HA Histórico de evolução:
+*     Versão  Autor    Data     Observações
+*     1       tbf   26/set/2016 início desenvolvimento.
+*
+*  $ED Descrição do módulo
+*
+*
+***************************************************************************/
 
+#include "Lista.h"
+
+#if defined( PECA_OWN )
+#define PECA_EXT
+#else
+#define PECA_EXT extern
+#endif
+
+
+/***********************************************************************
+*
+*  $TC Tipo de dados: PEC Condições de retorno
+*
+*
+*  $ED Descrição do tipo
+*     Condições de retorno das funções de uma PECA
+*
+***********************************************************************/
+
+typedef enum {
+
+	PEC_CondRetOK,
+	/* Concluiu corretamente  = 0*/
+
+	PEC_CondRetListaVazia,
+	/* A lista não contém elementos = 1 */
+
+	PEC_CondRetFimLista,
+	/* Foi atingido o fim de lista  = 2*/
+
+	PEC_CondRetNaoAchou,
+	/* Não encontrou o valor procurado = 3*/
+
+	PEC_CondRetFaltouMemoria
+	/* Faltou memória ao tentar criar um elemento de lista = 4 */
+
+} PEC_tpCondRet;
+
+
+//Estruturas-----------------------------------------
+typedef struct PEC_tagPeca * ppPeca;
+//Estruturas-----------------------------------------
+
+
+#undef TABULEIRO_EXT
+
+/********** Fim do módulo de definição: TAB  Tabuleiro **********/
+
+#else
+#endif
